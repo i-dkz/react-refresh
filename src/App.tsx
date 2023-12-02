70ddb5b2b25b4398e625ac8d6d66c9cbd34a5498
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-01 23:57:03 -0800

    backend stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,93 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import ProductList from "./components/ProductList";
+import axios, { AxiosError, CanceledError } from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [category, setCategory] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const controller = new AbortController();
 
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = {
+      id: 0,
+      name: "Zach Wong",
+    };
+
+    setUsers([newUser, ...users]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users/" + newUser)
+      .then(({ data : savedUser}) => setUsers([savedUser, ...users]))
+      .catch(err => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
 
   return (
     <>
       <NavBar />
       <main>
-        <select className="form-select" onChange={(e) => setCategory(e.target.value)}>
-          <option value=""></option>
-          <option value="Clothing">Clothing</option>
-          <option value="Household">Household</option>
-        </select>
-        <ProductList category={category}/>
+        {error && <p className="text-danger">{error}</p>}
+        {isLoading && <div className="spinner-border"></div>}
+        <button className="btn btn-primary mb-3" onClick={addUser}>
+          Add
+        </button>
+        <ul className="list-group">
+          {users.map((user) => (
+            <li
+              className="list-group-item d-flex justify-content-between"
+              key={user.id}
+            >
+              {user.name}
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
