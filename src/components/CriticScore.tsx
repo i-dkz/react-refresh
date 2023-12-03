88724b73afdee80340e8981b4cdc9f1430ddbd10
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-03 11:35:41 -0800

    created metacritic scores

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Tag } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 85 ? "green" : score > 65 ? "yellow" : "red";
+
+  return (
+    <>
+      <Tag variant="solid" colorScheme={color} color={color + ".200"}>
+        {score}
+      </Tag>
+    </>
+  );
+};
+
+export default CriticScore;
