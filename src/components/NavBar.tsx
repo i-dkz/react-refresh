1ec0d7fa44769e3dadafa5950ca7e95c358b1164
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-04 15:52:55 -0800

    implemented search function

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,14 +3,19 @@ import { IoIosAddCircle } from "react-icons/io";
 import Toggle from "./Toggle";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({onSearch} : Props) => {
   return (
     <HStack align={"center"} h={14} p={4} mb={4}>
       <IoIosAddCircle size={40} />
-        <SearchInput />
+        <SearchInput onSearch={onSearch}/>
         <Toggle />
     </HStack>
   );
 };
 
 export default NavBar;
+
